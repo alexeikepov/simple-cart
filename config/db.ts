c260ddd595d8624c35915f95106b87d1507feb2d
aviv <aviv1090@gmail.com>
2025-10-28 01:46:21 +0200

    neon connetion

--- config/db.ts
+++ config/db.ts
@@ -1,8 +1,12 @@
 import knex from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DB_PROD_URL,
+    ssl: { rejectUnauthorized: false },
   },
 });
