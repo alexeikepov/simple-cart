b6ef79e28ce3800d68bde8f3917d7413ddcd8228
aviv <aviv1090@gmail.com>
2025-10-28 12:05:03 +0200

    user cookie err handleing

--- auth/db.ts
+++ auth/db.ts
@@ -5,6 +5,11 @@ import { cookies } from "next/headers";
 export async function getUser() {
   const cookieStore = await cookies();
   const userId = cookieStore.get("user_id")?.value;
+  if (!userId || isNaN(Number(userId))) {
+    console.log("No valid user_id cookie found. Returning null.");
+    return null;
+  }
   const user = await db<User>("users").where("id", Number(userId)).first();
+  console.log("user", user);
   return user;
 }
