94e5437f7531d95dc60bc35b5155c6a8e1dda78c
alexeikepov <alexeikepov@gmail.com>
2025-10-28 18:01:36 +0200

    refactor: enhance CartFilter component and add database query for active carts

--- app/cartFilter/page.tsx
+++ app/cartFilter/page.tsx
@@ -1,10 +1,15 @@
 import { getCarts } from "@/cart/db";
 import ExampleTable from "@/components/ExamleTable";
+import { db } from "@/config/db";
 
 export default async function СartFilter({ searchParams }) {
   const filter = await searchParams;
   const carts = await getCarts();
-  console.log(carts, "carts");
+
+  const res = await db("orders");
+
+  console.log("res", res);
+
   return (
     <div>
       <ExampleTable data={carts} />
