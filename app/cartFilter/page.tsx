af656debaeec1365516360f0817bd7e19b7a0eff
alexeikepov <alexeikepov@gmail.com>
2025-10-29 09:12:59 +0200

    refactor: remove unused imports in api.ts, page.tsx, and Prdct.tsx

--- app/cartFilter/page.tsx
+++ app/cartFilter/page.tsx
@@ -1,14 +1,9 @@
 import { getCarts } from "@/cart/db";
 import ExampleTable from "@/components/ExamleTable";
-import { db } from "@/config/db";
 
 export default async function СartFilter({ searchParams }) {
   const filter = await searchParams;
   const carts = await getCarts();
-
-  const res = await db("orders");
-
-  console.log("res", res);
 
   return (
     <div>
